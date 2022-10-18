7f17fe544995efff6f424cfb28a710cebf418fe4
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2022-10-18 18:10:26 -0300

    More tags

--- dinosoar/urls.py
+++ dinosoar/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from tkinter.messagebox import showerror
+
 from django.contrib import admin
 from django.urls import path
 
 from dinofacts.views import show_dino
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('show_dino/<str:name>/', show_dino)
+    path("admin/", admin.site.urls),
+    path("show_dino/<str:name>/", show_dino),
 ]
